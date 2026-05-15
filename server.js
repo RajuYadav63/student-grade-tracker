872b14c38548eca068461d688ff8b45c6a7d204e
Raju Yadav <raju.yadav@example.com>
2026-05-15 13:49:26 +0530

    fixed render module issue

--- server.js
+++ server.js
@@ -1,75 +1,41 @@
-const API = "https://student-grade-tracker-aw8o.onrender.com";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express();
 
-// READ
-async function getUsers() {
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
-    const res = await fetch(`${API}/users`);
+mongoose.connect("mongodb+srv://yadavraju76878_db_user:raju6367@cluster0.8mc0wnx.mongodb.net/?appName=Cluster0")
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch((err) => console.log(err));
 
-    const users = await res.json();
+const User = require("./models/user");
 
-    let output = "";
+app.get("/", (req, res) => {
+    res.send("Student Grade Tracker API Running");
+});
 
-    users.forEach((user) => {
+app.post("/add", async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.json(user);
+});
 
-        output += `
-        
-        <div class="user">
+app.get("/users", async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
 
-            <h3>${user.name}</h3>
+app.delete("/delete/:id", async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "Deleted" });
+});
 
-            <p>${user.email}</p>
+const PORT = process.env.PORT || 5000;
 
-            <button onclick="deleteUser('${user._id}')">
-                Delete
-            </button>
-
-        </div>
-        `;
-    });
-
-    document.getElementById("userList").innerHTML = output;
-}
-
-
-
-// CREATE
-async function addUser() {
-
-    const name = document.getElementById("name").value;
-
-    const email = document.getElementById("email").value;
-
-    await fetch(`${API}/add`, {
-
-        method: "POST",
-
-        headers: {
-            "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify({
-            name,
-            email
-        })
-    });
-
-    getUsers();
-}
-
-
-
-// DELETE
-async function deleteUser(id) {
-
-    await fetch(`${API}/delete/${id}`, {
-
-        method: "DELETE"
-
-    });
-
-    getUsers();
-}
-
-
-getUsers();
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`);
+});
